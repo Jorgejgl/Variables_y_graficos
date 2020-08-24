abb755a93de5225e7e2fbdb0a2c9187df148a8e7
Jorge <jorge.gonzalezl@utem.cl>
2020-08-23 21:33:48 -0400

    Tipos de variables y gráficos.
    Información sobre estadísticas y tipos de gráficos.

---
+++ Información_gráficos.R
@@ -0,0 +1,95 @@
+## INVESTIGACIÓN GRÁFICOS.
+
+
+## Tipos de variables. 
+
+# CUALITATIVAS: se refieren a cualidades, las que no pueden ser expresadas
+# numericamente. Pueden ser ordinales o categóricas.
+# Ordinales: siguen un orden o secuencia. (Ej: Meses del año o abedecedario)
+# Nominales: no siguen un orden. (Ej: Estado civil o comuna)
+
+# CUANTITATIVAS: se refieren a cantidades o valores númericos. Pueden ser 
+# discretas o continuas.
+# Discretas: si toman valores enteros. (Ej: número de hijos o alumnos)
+# Continuas: pueden tomar un valor entre intervalo. (Ej: Estatura o peso.)
+
+
+## ¿Que es un gráfico?
+
+# Un gráfico es una representación visual de una serie de datos estadísticos.
+# Para elegir un gráfico se debe pensar en que se desea ilustrar, considerando
+# que cada tipo de gráfico resalta aspectos diferentes.
+
+
+## Tipos de gráficos.
+
+# Gráfico de barras o de columnas.
+# Histograma.
+# Gráfico de lineas.
+# Poligonos de frecuencia.
+# Box-Plot
+# Gráfico circular o de torta.
+# Gráfico de dispersión.
+# Cartograma
+
+
+## Tipos de gráficos: definición.
+
+# Gráfico de barras: es la representación gráfica de las frecuencias
+# de una variable cualitativa o discreta.
+# En uno de los ejes se posicionan las distintas categorías y en el otro eje 
+# el valor o frecuencia de cada categoría en una determinada escala.
+# La orientación del gráfico puede ser horizontal o vertical.
+# El gráfico de barras se utiliza principalmente para: comparar magnitudes
+# de varias categorias o ver la evolución en el tiempo una variable.
+
+# Histograma: se utiliza para representar las frecuencias de una variable
+# cuantitativa continua.
+# En uno de los ejes se posicionan los intervalos de la variable continua
+# y en el otro eje las frecuencias. No existe separación entre las barras.
+
+# Gráfico de lineas: es una representación de la relación que existe 
+# entre 2 variables y que refleja con claridad los cambios producidos.
+# En cada eje (x e y), se representa cada una de las variables cuya
+# relación se quiere observar.
+# Se suele usar para presentar tendencias temporales, lo cual se logra
+# posicionando en el "eje x" la variable que indica las unidades de tiempo  
+# y en el "eje y" la escala de la variable cuya variación en el tiempo
+# se quiere ver.
+
+# Polígono de frecuencias: es un gráfico lineal que se obtiene al unir los
+# puntos medios (marcas de clase), de las bases superiores de las barras 
+# presentes en gráficos de barras o histogramas.
+
+# Box-Plot: es una representación gráfica que resalta aspectos de la 
+# distribución de las observaciones en una o más series de datos
+# cuantitativos. Permite observar los cuartiles de los datos, la mediana 
+# y valores atípicos.
+# El box-plot utiliza una sola escala, que es la correspondiente a la 
+# variable de los datos que se presentan.
+
+# Gráfico circular: es una representación circular de las frecuencias
+# relativas de una variable cualitativa o discreta, lo que permite
+# su comparación.
+# El circulo representa la totalidad que se quiere observar y cada porción
+# representan la proporción de cada categoría en la variable con respecto
+# del total. Suele expresarse en porcentajes.
+# Un gráfico circular es útil cuando las categorías son pocas, debido a
+# que si son muchas, no aportaría información clara y sería incomprensible.
+
+# Gráfico de dispersión: muestra en un eje cartesiano la relación que
+# existe entre 2 variables.
+# Este gráfico es util para examinar el grado de correlación entre las
+# 2 variables, es decir, nos muestra si el incremento o disminución de los
+# valores de una de las variables, denominada variable independiente y 
+# que se suele representar en el "eje x", altera de alguna manera los 
+# valores de la otra, denominada variable dependiente y que se representa
+# generalmente en el "eje y".
+# El tipo de correlación se puede deducir según la forma de la nube de 
+# puntos y puede ser: correlación nula, correlación lineal o correlación
+# no lineal.
+
+# Cartograma: es un mapa que muestra datos cuantitativos asociados a
+# regiones, mediante la modificación de los tamaños de las unidades de
+# enumeración.
+
